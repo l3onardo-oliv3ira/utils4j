e159e514c653b6fadbf5ba94e8508d2c42360921
Leonardo de Lima Oliveira <l3onardo-oliv3ira@gmail.com>
2022-08-16 04:59:19 -0400

    snapshot prefix

--- src/main/java/com/github/utils4j/imp/Directory.java
+++ src/main/java/com/github/utils4j/imp/Directory.java
@@ -1,28 +1,28 @@
 /*
-* MIT License
-* 
-* Copyright (c) 2022 Leonardo de Lima Oliveira
-* 
-* https://github.com/l3onardo-oliv3ira
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-*/
+ * MIT License
+ * 
+ * Copyright (c) 2022 Leonardo de Lima Oliveira
+ * 
+ * https://github.com/l3onardo-oliv3ira
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 
 package com.github.utils4j.imp;
@@ -31,57 +31,60 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.Comparator;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public abstract class Directory {
   private Directory() {}
-  
+
   public static void rmDir(Path path) throws IOException {
     rmDir(path, f -> true);
   }
-  
+
   public static void mkDir(Path path) throws IOException {
     Args.requireNonNull(path, "path is null");
     mkDir(path.toFile());
   }
-  
+
   public static void requireDirectory(File dir, String message) throws IOException {
     Args.requireNonNull(dir, "input is null");
     if (!dir.isDirectory())
       throw new IOException(message);
   }
-  
+
   public static File requireNotExists(File input, String message) throws IOException {
     Args.requireNonNull(input, "input is null");
     if (input.exists()) 
       throw new IOException(message);
     return input;
   }
-  
+
   public static File requireExists(File input, String message) throws IOException {
     Args.requireNonNull(input, "input is null");
     if (!input.exists())
       throw new IOException(message);
     return input;
   }
-  
+
   public static void rmDir(Path path, Predicate<File> predicate) throws IOException {
     Args.requireNonNull(path, "path is null");
     Args.requireNonNull(predicate, "predicate is null");
-    try (Stream<Path> walk = Files.walk(path)) {        
-      walk.sorted(Comparator.reverseOrder())
+    if (Files.exists(path)) {
+      try (Stream<Path> walk = Files.walk(path)) {        
+        walk.sorted(Comparator.reverseOrder())
         .map(Path::toFile)
         .filter(predicate)
         .forEach(File::delete);
+      }
     }
   }
-  
+
   public static File createTempFile(String prefix) throws IOException {
     return createTempFile(prefix, ".util4j.tmp");
   }
-  
+
   public static File createTempFile(String prefix, File directory) throws IOException {
     return createTempFile(prefix, ".util4j.tmp", directory);
   }
@@ -89,7 +92,7 @@ public abstract class Directory {
   public static File createTempFile(String prefix, String suffix) throws IOException {
     return createTempFile(prefix, suffix, null);
   }
-  
+
   public static File createTempFile(String prefix, String suffix, File directory) throws IOException {
     Args.requireNonNull(prefix, "prefix is null");
     try {
@@ -111,4 +114,22 @@ public abstract class Directory {
       throw new IOException("Unabled to create directory tree  " + folder);
     }
   }  
+
+  public static void copyFolder(Path src, Path dest) throws IOException {
+    Args.requireNonNull(src, "src is null");
+    Args.requireNonNull(dest, "dest is null");
+    try (Stream<Path> stream = Files.walk(src)) {
+      stream.forEach(source -> copy(source, dest.resolve(src.relativize(source))));
+    }catch(RuntimeException e) {
+      throw new IOException(e);
+    }
+  }
+
+  private static void copy(Path source, Path dest) {
+    try {
+      Files.copy(source, dest, StandardCopyOption.REPLACE_EXISTING);
+    } catch (Exception e) {
+      throw new RuntimeException(e.getMessage(), e);
+    }
+  }
 }
